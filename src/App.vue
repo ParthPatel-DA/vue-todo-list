a90265895c88a6afb3bbc0f25701daf906769e0b
Parth Patel <parthpatel454500@gmail.com>
2022-04-05 13:33:42 +0530

    Add, Edit, Delete and list

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form @submit.prevent="addTask">
+      <input v-model.trim="todoText" />
+      <button type="submit" style="margin-left: 10px">
+        {{ editIndex === -1 ? "Add" : "Edit" }} Task
+      </button>
+      <button
+        type="reset"
+        v-show="editIndex !== -1"
+        style="margin-left: 10px"
+        @click="editIndex = -1"
+      >
+        Keep old
+      </button>
+    </form>
+    <hr />
+    <div class="task-list">
+      <h3>Task List</h3>
+      <table>
+        <tr
+          is="task-card"
+          v-for="(task, index) in todoData"
+          :key="task.id"
+          :task="task"
+          :index="index"
+          @edit="editTask"
+          @delete="deleteTask"
+        />
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskCard from "./components/TaskCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TaskCard,
+  },
+  data: function () {
+    return {
+      todoText: "",
+      todoData: [],
+      currentIndex: 1,
+      editIndex: -1,
+    };
+  },
+  methods: {
+    addTask: function () {
+      if (this.todoText.length === 0) {
+        alert("Please add task");
+      } else {
+        if (this.editIndex !== -1) {
+          this.todoData[this.editIndex].text = this.todoText;
+          this.editIndex = -1;
+        } else {
+          this.todoData.push({ text: this.todoText, id: this.currentIndex });
+          this.currentIndex++;
+        }
+        this.todoText = "";
+      }
+    },
+    editTask: function (index) {
+      this.todoText = this.todoData[index].text;
+      this.editIndex = index;
+    },
+    deleteTask: function (index) {
+      this.todoData = this.todoData.filter((_, i) => index !== i);
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,8 +79,5 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <tr>
+    <td>{{ task.text }}</td>
+    <td><button @click="$emit('edit', index)">Edit</button></td>
+    <td><button @click="$emit('delete', index)">Delete</button></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "TaskCard",
+  props: {
+    task: Object,
+    index: Number,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
